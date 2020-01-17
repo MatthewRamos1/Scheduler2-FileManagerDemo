db996454a9dcc90b5663b85cbb0495d6cf0369cb
Matthew Ramos <mmramos309@gmail.com>
2020-01-17 10:56:03 -0500

    Organized files into groups, created file manager extension file

---
+++ Scheduler/FileManager + Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  FileManager + Extensions.swift
+//  Scheduler
+//
+//  Created by Matthew Ramos on 1/17/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+extension FileManager {
+    
+}
